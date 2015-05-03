3d75c95f130cd1140c037411240fbe6cd10bed21
Ralph Moser <ralph@ramosoft.ch>
2015-05-03 20:55:23 +0200

    - menu integrated - hso anmeldung corrected (webform id's, webform v3->v4) - db updated

--- all/themes/pixelgarage/templates/page.tpl.php
+++ all/themes/pixelgarage/templates/page.tpl.php
@@ -95,17 +95,19 @@
 
     <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
       <div class="navbar-collapse collapse">
-        <nav role="navigation">
+        <nav class="main-navigation clearfix" role="navigation">
           <?php if (!empty($primary_nav)): ?>
             <?php print render($primary_nav); ?>
           <?php endif; ?>
           <?php if (!empty($secondary_nav)): ?>
             <?php print render($secondary_nav); ?>
           <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
         </nav>
+        <?php if (!empty($page['navigation'])): ?>
+          <nav class="sub-navigation clearfix" role="navigation">
+            <?php print render($page['navigation']); ?>
+          </nav>
+        <?php endif; ?>
       </div>
     <?php endif; ?>
   </div>
