13bb9fca540366ed1c40ebe7d7ce01618968311a
Ralph Moser <ralph@ramosoft.ch>
2015-05-07 00:16:46 +0200

    - LESS implementation continued - db updated

--- all/themes/pixelgarage/js/pxlBehaviors.js
+++ all/themes/pixelgarage/js/pxlBehaviors.js
@@ -7,50 +7,43 @@
 (function ($) {
 
     /**
-     * Allows full size clickable items.
+     * Generates tabs for header links.
      */
-    Drupal.behaviors.fullSizeClickableItems = {
-        attach: function () {
-            var $clickableItems = $('.node-link-item.node-teaser .field-group-div')
-                .add('.node-team-member.node-teaser .field-group-div');
-
-            $clickableItems.once('click', function () {
-                $clickableItems.on('click', function () {
-                    window.location = $(this).find("a:first").attr("href");
-                    return false;
+    Drupal.behaviors.block_with_header_links = {
+        attach: function (context) {
+            $('.block-views-contacts-block-course-contacts, #block-views-locations-block', context).once(function() {
+                var _header = $('<ul class="header-links" />');
+                $(this).find('.view .item-list>h3').each(function(i, e) {
+                    var _s = $(this);
+                    var _l = $('<li />').html('<a href="#" data-i="' + i + '">' + _s.html() + '</a>');
+                    _header.append(_l);
+                    _s.remove();
                 });
+                $(this).find('.view').prepend(_header);
+                $(this).find('.header-links a').click(function() {
+                    $(this).closest('ul').find('a').removeClass('active');
+                    $(this).addClass('active').closest('.view').find('.item-list').hide().filter(':eq(' + $(this).data('i') + ')').show();
+                    return false;
+                }).filter(':first').click();
+            });
+        }
+    }
+
+    /**
+     * This behavior adds shadow to header on scroll.
+     *
+     */
+    Drupal.behaviors.addHeaderShadow = {
+        attach: function (context) {
+            $(window).on("scroll", function() {
+                if ($(window).scrollTop() > 10) {
+                    $("header.navbar .container").css( "box-shadow", "0 4px 3px -4px gray");
+                } else {
+                    $("header.navbar .container").css( "box-shadow", "none");
+                }
             });
         }
     };
-
-    /**
-     * Swaps images from black/white to colored on mouse hover.
-     */
-    Drupal.behaviors.hoverImageSwap = {
-        attach: function () {
-            $('.node-project.node-teaser .field-name-field-images a img').hover(
-                function () {
-                    // mouse enter
-                    src = $(this).attr('src');
-                    $(this).attr('src', src.replace('teaser_bw', 'teaser_normal'));
-                },
-                function () {
-                    // mouse leave
-                    src = $(this).attr('src');
-                    $(this).attr('src', src.replace('teaser_normal', 'teaser_bw'));
-                }
-            );
-        }
-    }
-
-    /**
-     * Open file links in its own tab. The file field doesn't implement this behaviour right away.
-     */
-    Drupal.behaviors.openDocumentsInTab = {
-        attach: function () {
-            $(".field-name-field-documents").find(".field-item a").attr('target', '_blank');
-        }
-    }
 
 
 
